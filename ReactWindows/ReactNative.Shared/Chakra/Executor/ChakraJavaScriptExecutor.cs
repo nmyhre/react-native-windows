a7feb773d4000edefcd00b95601f53ce8c6f4b02
Nathaniel Myhre <nmyhre@microsoft.com>
2018-03-24 13:02:19 -0700

    Set debug mode during debug builds of Chakra.

--- ReactWindows/ReactNative.Shared/Chakra/Executor/ChakraJavaScriptExecutor.cs
+++ ReactWindows/ReactNative.Shared/Chakra/Executor/ChakraJavaScriptExecutor.cs
@@ -240,7 +240,10 @@ namespace ReactNative.Chakra.Executor
             try
             {
                 _context = JavaScriptSourceContext.Increment(_context);
-                JavaScriptContext.RunScript(script, _context, sourceUrl);
+                #if DEBUG
+                JavaScriptContext.StartDebugging();
+                #endif
+            JavaScriptContext.RunScript(script, _context, sourceUrl);
             }
             catch (JavaScriptScriptException ex)
             {
