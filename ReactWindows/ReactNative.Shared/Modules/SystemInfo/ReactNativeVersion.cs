46c06278b97a7ab58efacc8a65d8e67b50346f09
Nathaniel Myhre <nmyhre@microsoft.com>
2018-03-24 13:04:49 -0700

    Temporary fix for version checking.

--- ReactWindows/ReactNative.Shared/Modules/SystemInfo/ReactNativeVersion.cs
+++ ReactWindows/ReactNative.Shared/Modules/SystemInfo/ReactNativeVersion.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Portions derived from React Native:
 // Copyright (c) 2015-present, Facebook, Inc.
 // Licensed under the MIT License.
@@ -22,7 +22,7 @@ namespace ReactNative.Modules.SystemInfo
                 return new JObject
                 {
                     { "major", 0 },
-                    { "minor", 0 },
+                    { "minor", 54 },
                     { "patch", 0 },
                     { "prerelease", null }
                 };
